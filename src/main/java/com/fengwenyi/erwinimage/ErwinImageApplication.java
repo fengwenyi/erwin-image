dc39f7bf16e31dbb75d090820f0ceaa57bee5623
Erwin <xfsy_2015@163.com>
2021-11-30 20:22:39 +0800

    first commit

---
+++ src/main/java/com/fengwenyi/erwinimage/ErwinImageApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwinimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErwinImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErwinImageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengwenyi/erwinimage/ErwinImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwinimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErwinImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
